b518d38f8c0e97c54d7f4fe5e51d4ec018f0d6d8
Francis Yáñez <francis.yanez@nazaries.com>
2018-05-02 01:29:56 +0200

    Updated deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "yanez.io"
+set :repo_url, "git@github.com/fjyaniez/yanez.io.git"
+set :repo_tree, '_site'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, '/home/web'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
